42b8756cde134e014d74e9bff8acb1cf69566ff4
lara huang <lara1105huang@gmail.com>
2024-09-06 09:30:18 +0800

    Tailwind 安裝

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,16 @@ import HelloWorld from './components/HelloWorld.vue'
 
   <router-view></router-view>
   <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <div class="p-6 max-w-sm mx-auto rounded-xl shadow-lg flex items-center space-x-4 bg-slate-900 p-8 pt-7">
+
+  <div>
+    <div class="text-xl font-medium text-white">ChitChat</div>
+    <p class="text-white">You have a new message!</p>
+  </div>
+</div>
 </template>
 
 <style scoped>
