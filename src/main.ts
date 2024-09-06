fa4c15a3127d8e5324aef4dcbd924a95221c34ee
lara huang <lara1105huang@gmail.com>
2024-09-06 09:41:15 +0800

    安裝Sass與載入

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
-import './style.css' 
 import App from './App.vue'
 import router from "./router/router"
+import './style.css' 
+import './assets/hover-css/hover-min.css' 
+import './assets/fontawesome-css/css/fontawesome.min.css' 
+import './assets/icomoon/style.css' 
+import './assets/scss/allStyle.scss'
 
 const app = createApp(App);
     app.use(router);
